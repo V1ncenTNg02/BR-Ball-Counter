62f5674aba2d6684dcff227a141a6b601d6a345f
V1ncentNg <112238267+V1ncentNg@users.noreply.github.com>
2024-09-19 01:42:27 +1000

    connect google postgres

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const { Pool } = require('pg');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
+
+const pool = new Pool({
+  user: 'vincent',
+  host: '34.129.133.215', 
+  database: 'postgres', 
+  password: 'vincent',
+  port: 5432, 
+});
 
 app.post('/user', async (req,res) => {
-
+  const {username, redball, blueball} = req.body;
+  try{
+    const result = await pool.query(
+      'INSERT INTO postgres (names, redball, blueball) VALUES ($1, $2, $3) RETURNING *',
+      [username, redball, blueball]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch(e){
+    res.status(400).json({error:e.message});
+  }
 })
 
 app.get('/stat', async (req, res) => {
