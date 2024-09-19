dd8f1fa394408a85b1a1df486806aac2e7b2e028
V1ncentNg <112238267+V1ncentNg@users.noreply.github.com>
2024-09-20 00:55:05 +1000

    removed unnecessary codes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Signin from "./pages/Signin";
 import Home from "./pages/Home";
 import Report from "./pages/Report";
